cbd215cee0afed424a9ab95b21895d34992fbe3e
rajatmittalc <52102595+rajatmittalc@users.noreply.github.com>
2020-10-01 16:03:46 +0530

    Add files via upload

---
+++ project 1 virtual assistant.py
@@ -0,0 +1,20 @@
+import wolframalpha
+import wikipedia
+
+while True:
+    input1 = input("Q:")
+    try:
+        app_id = 'HYPT97-JP7J3U4GPP'               #wolframealpha
+        client = wolframalpha.Client(app_id)
+        res = client.query(input1)
+        answer = next(res.results).text
+        print(answer)
+
+    except:
+                                           #wikipedia
+        wikipedia.set_lang("en")# change language as you like
+        input1 = input1.split(' ')
+        input1 = " ".join(input1[2:])
+        print(wikipedia.summary(input1,sentences = 10))
+
+
